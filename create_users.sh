9c709f925783de6723505871d6aac855221c1721
dmhernanandez <dany1999hernan@gmail.com>
2022-03-04 23:47:12 -0600

    Se agrego barra de progreso cuando se crean los usuarios

--- create_users.sh
+++ create_users.sh
@@ -3,21 +3,33 @@
 USER_NAME=""
 NAME_TO_SAVE=""
 count=0
+counter_progressbar=0
 TEMP_FILE='usradd_temp.txt'
 ORIGINAL_FILE='usersadd.txt'
 DELETE_USR_FILE='usersdelete.txt'
 IS_COMPLETE=0
 touch $TEMP_FILE
 cat $ORIGINAL_FILE > $TEMP_FILE
-while IFS= read -r FULLNAME
+
+#Para obtener la escala se lee la cantidad de filas que tiene el archivo, que equivale al numero de filas
+#luego se divide 100 entre la cantidad de lineas. Este valor se usa para crear el porcentaje de la barra de progeso
+cantidad=$(wc -l $ORIGINAL_FILE | awk '{print $1}')
+escala=$(echo "100.0/$cantidad" | bc)
+
+(while IFS= read -r FULLNAME
 do
 
    count=0
-   IS_COMPLETE=0
+   
+ 
+  sleep 1
+  echo 	XXX	#Delimitadores usados por whiptail
+  counter_progressbar=$((counter_progressbar+1))
+  echo $((counter_progressbar*escala))
+  IS_COMPLETE=0 
      #Cada linea se divide por espacio
    . getUsername.sh
-    adduser $USER_NAME 
-   
+    adduser $USER_NAME
    if [[ $IS_COMPLETE -eq 1 && $? -eq 0 ]]
    then 
        echo $USER_NAME >> usersdelete.txt
@@ -27,8 +39,8 @@ do
 	echo "Error al crear usuario $USER_NAME"
    
    fi
-  
+  echo XXX
 
-done < $TEMP_FILE
+done < $TEMP_FILE) | whiptail --gauge "Eliminando" 8 80 0
 rm $TEMP_FILE
 
