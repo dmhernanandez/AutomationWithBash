9c2e5e3f1ae9c7ab75254679f059efbe0f625047
dmhernanandez <dany1999hernan@gmail.com>
2022-03-11 01:16:51 -0600

    Se agrego script para instalar grafana

---
+++ grafana.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Install Grafana 6.7.2
+# Para CentOS 7/8 & Redhat
+yum install -y https://dl.grafana.com/oss/release/grafana-6.7.2-1.x86_64.rpm
+
+# Start service Grafana
+service grafana-server start
+
+#
+/sbin/chkconfig -add grafana-server
+
+#  start service automatically at boot 
+systemctl enable grafana-server
+
+# list many plugins
+grafana-cli plugins list-remote
+
+# Install plugin to zabbix in grafana
+grafana-cli plugins install alexanderzobnin-zabbix-app
+
+# Restart service Grafana
+service grafana-server restart
